613a6a73095a12d393fc8989b4cc35407f59c7f7
Marcus <58707230+torpeedoo@users.noreply.github.com>
2023-03-12 17:27:24 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -11,7 +11,7 @@ def request(content, author):
   headers = {
 	  "content-type": "application/json",
 	  "X-RapidAPI-Host": "waifu.p.rapidapi.com",
-	  "X-RapidAPI-Key": "a06f36d888mshac45a0eacccdc2cp1cbcd1jsn3f55394800a6"
+	  "X-RapidAPI-Key": ""
   }
 
   response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
@@ -20,7 +20,7 @@ def request(content, author):
 
 def reset():
   headers1 = {
-  	"X-RapidAPI-Key": "a06f36d888mshac45a0eacccdc2cp1cbcd1jsn3f55394800a6",
+  	"X-RapidAPI-Key": "",
   	"X-RapidAPI-Host": "waifu.p.rapidapi.com"
   }
 
